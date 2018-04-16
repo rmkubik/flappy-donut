737b4f64132d8b34e20334c7f163e4d2d0fd7619
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-04-15 17:00:41 -0700

    Hello world

---
+++ index.js
@@ -0,0 +1,45 @@
+import 'phaser';
+import './src/test';
+import tree1 from './assets/tree1.png';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 200
+            }
+        }
+    },
+    scene: {
+        preload,
+        create
+    }
+}
+
+const gameEl = document.querySelector(`#${config.parent}`);
+const game = new Phaser.Game(config);
+
+function preload() {
+    console.log('preload');
+    this.load.image('test', tree1);
+}
+
+function create() {
+    const text = this.add.text(100, 100, 'Phaser + Parcel');
+
+    this.add.image(400, 100, 'test');
+}
+
+if (module.hot) {
+  module.hot.accept(() => {
+    while (gameEl.firstChild) {
+      gameEl.removeChild(gameEl.firstChild);
+    }
+    game.boot();
+  });
+}
