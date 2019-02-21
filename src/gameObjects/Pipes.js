9d0af49a5442a7ada5d1c6c0458b18479f95eadb
Ryan Kubik <rmkubik@me.com>
2019-02-20 22:37:56 -0800

    Clean up pipes and mainscene

---
+++ src/gameObjects/Pipes.js
@@ -0,0 +1,81 @@
+import Phaser from "phaser";
+import globals from "../globals";
+
+class Pipes extends Phaser.GameObjects.Group {
+  constructor({ scene }) {
+    super(scene);
+    scene.add.existing(this);
+    this.scene = scene;
+
+    this.startSpawnLoop();
+  }
+
+  startSpawnLoop() {
+    this.scene.time.addEvent({
+      delay: 1000,
+      callback: this.buildPipeSegmentPair,
+      callbackScope: this,
+      loop: true
+    });
+  }
+
+  create({ x, y, sprite, ceiling }) {
+    const pipe = super.create(x, y, "donuts", sprite);
+    this.scene.physics.world.enable(pipe);
+    pipe.setScale(globals.scale);
+    pipe.body.velocity.x = -300;
+    pipe.flipY = ceiling;
+
+    return pipe;
+  }
+
+  static isPipeOffScreen(pipe) {
+    return pipe.body.x < 0 || pipe.body.y > globals.height;
+  }
+
+  update() {
+    this.getChildren().forEach(pipe => {
+      if (Pipes.isPipeOffScreen(pipe)) {
+        if (pipe.active) {
+          this.remove(pipe);
+        }
+      }
+    });
+  }
+
+  buildPipeSegment(height, ceiling) {
+    for (let i = 1; i <= height; i += 1) {
+      const pipeLength = globals.tileSize * globals.scale * i;
+
+      let y;
+      if (ceiling) {
+        y = pipeLength - globals.tileSize;
+      } else {
+        y = globals.height - pipeLength + globals.tileSize;
+      }
+
+      this.create({
+        x: globals.width + (globals.tileSize / 2) * globals.scale,
+        y,
+        sprite: i === height ? 1 : 2,
+        ceiling
+      });
+    }
+  }
+
+  /**
+   * Build a pipe on the screen that is either attached to the floor or the ceiling
+   */
+  buildPipeSegmentPair() {
+    const topHeight = Phaser.Math.RND.between(
+      1,
+      globals.tilesTall - globals.pipeGapSize - 1
+    );
+    const bottomHeight = globals.tilesTall - globals.pipeGapSize - topHeight;
+
+    this.buildPipeSegment(topHeight, false);
+    this.buildPipeSegment(bottomHeight, true);
+  }
+}
+
+export default Pipes;
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,100 +1,48 @@
 import Phaser from "phaser";
 import globals from "../globals";
-import donutImg from "../../assets/donut.svg";
-import treeImg from "../../assets/tree1.png";
-import donutsImg from "../../assets/donuts.png";
+import donutSpriteSheet from "../../assets/donuts.png";
 import Donut from "../gameObjects/Donut";
+import Pipes from "../gameObjects/Pipes";
 
 class MainScene extends Phaser.Scene {
   preload() {
-    this.load.image("donut", donutImg);
-    this.load.image("tree", treeImg);
-    this.load.spritesheet("donuts", donutsImg, {
+    this.load.spritesheet("donuts", donutSpriteSheet, {
       frameWidth: globals.tileSize,
       frameHeight: globals.tileSize
     });
   }
 
   create() {
-    const text = this.add.text(10, 10, "Flappy Donut");
+    this.add.text(10, 10, "Flappy Donut");
 
     this.donut = new Donut({ scene: this, position: { x: 150, y: 100 } });
 
-    this.pipes = this.physics.add.group();
-    this.buildPipe(5);
+    this.pipes = new Pipes({ scene: this });
+    this.pipes.buildPipeSegment(5);
 
     this.physics.add.collider(this.donut, this.pipes, (donut, pipe) => {
       pipe.body.setGravityY(globals.gravity);
     });
 
-    this.input.keyboard.on("keydown_SPACE", event => {
+    this.createPlayerInputs();
+  }
+
+  createPlayerInputs() {
+    this.input.keyboard.on("keydown_SPACE", () => {
       this.donut.flap();
     });
 
-    this.input.on("pointerdown", event => {
+    this.input.on("pointerdown", () => {
       this.donut.flap();
     });
 
-    this.input.keyboard.on("keydown_ENTER", event => {
-      this.buildPipePair();
-    });
-
-    this.time.addEvent({
-      delay: 1000,
-      callback: this.buildPipePair,
-      callbackScope: this,
-      loop: true
+    this.input.keyboard.on("keydown_ENTER", () => {
+      this.buildPipeSegmentPair();
     });
   }
 
   update() {
-    this.pipes.getChildren().forEach(pipe => {
-      if (pipe.body.x < 0 || pipe.body.y > globals.height) {
-        if (pipe.active) {
-          this.pipes.remove(pipe);
-        }
-      }
-    });
-  }
-
-  buildPipe(height, ceiling) {
-    for (let i = 1; i <= height; i += 1) {
-      let y;
-      let flipY;
-      if (ceiling) {
-        y = globals.tileSize * globals.scale * i - globals.tileSize;
-        flipY = true;
-      } else {
-        y =
-          globals.height -
-          globals.tileSize * globals.scale * i +
-          globals.tileSize;
-        flipY = false;
-      }
-
-      const pipe = this.pipes.create(
-        globals.width + (globals.tileSize / 2) * globals.scale,
-        y,
-        "donuts",
-        i === height ? 1 : 2
-      );
-      pipe.setScale(globals.scale);
-      pipe.body.velocity.x = -300;
-      pipe.flipY = flipY;
-    }
-  }
-
-  /**
-   * Build a pipe on the screen that is either attached to the floor or the ceiling
-   */
-  buildPipePair() {
-    const topHeight = Phaser.Math.RND.between(
-      1,
-      globals.tilesTall - globals.pipeGapSize - 1
-    );
-    const bottomHeight = globals.tilesTall - globals.pipeGapSize - topHeight;
-    this.buildPipe(topHeight);
-    this.buildPipe(bottomHeight, true);
+    this.pipes.update();
   }
 }
 
