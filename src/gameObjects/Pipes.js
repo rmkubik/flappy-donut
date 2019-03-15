c7382ff692693d06b4f0295cce0a0979e60a3e74
Ryan Kubik <rmkubik@me.com>
2019-03-14 17:44:35 -0700

    Rename vars to match starter

--- src/gameObjects/Pipes.js
+++ src/gameObjects/Pipes.js
@@ -8,12 +8,12 @@ class Pipes extends Phaser.GameObjects.Group {
     this.scene = scene;
   }
 
-  create({ x, y, sprite, ceiling }) {
+  create({ x, y, sprite, isOnCeiling }) {
     const pipe = super.create(x, y, "donuts", sprite);
     this.scene.physics.world.enable(pipe);
     pipe.setScale(globals.scale);
     pipe.body.velocity.x = -300;
-    pipe.flipY = ceiling;
+    pipe.flipY = isOnCeiling;
 
     return pipe;
   }
@@ -32,12 +32,12 @@ class Pipes extends Phaser.GameObjects.Group {
     });
   }
 
-  buildPipe(height, ceiling) {
+  buildPipe(height, isOnCeiling) {
     for (let i = 1; i <= height; i += 1) {
       const pipeLength = globals.tileSize * globals.scale * i;
 
       let y;
-      if (ceiling) {
+      if (isOnCeiling) {
         y = pipeLength - globals.tileSize;
       } else {
         y = globals.height - pipeLength + globals.tileSize;
@@ -47,7 +47,7 @@ class Pipes extends Phaser.GameObjects.Group {
         x: globals.width + (globals.tileSize / 2) * globals.scale,
         y,
         sprite: i === height ? 1 : 2,
-        ceiling
+        isOnCeiling
       });
     }
   }
